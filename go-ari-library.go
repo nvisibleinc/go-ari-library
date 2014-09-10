79f2b6b3a47495458c9ce580648bc1eeab13cac7
blitzrage <blitzrage@n-visibleinc.com>
2014-09-10 13:42:17 +0000

    Add Init function with in and out channels.
    Move this to the nv package and make it look more like a library.

---
+++ go-ari-library.go
@@ -0,0 +1,23 @@
+package nv
+
+
+import (
+  "encoding/json"
+  "github.com/bitly/go-nsq"
+)
+
+type NV_Event struct {
+    ServerID    string
+    Timestamp   time.Time
+    ARI_Event   string
+}
+
+func Init(in chan string, out chan *NV_Event) {
+  go func(in chan string, out chan *NV_Event) {
+    for instring := range in {
+      var e *NV_Event
+      json.Unmarshal([]byte(instring), e)
+      out <- e
+    }
+  }(in, out)
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-  "json"
-)
-
-func main() {
-  
-}
